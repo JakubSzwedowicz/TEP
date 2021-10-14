67ca0c9fc4d0f6a92c057e67c7e4cf699fca4e44
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2021-10-14 18:06:57 +0200

    Added first test for Utilities

--- List_1/test/tests/Test_Utilities.cpp
+++ List_1/test/tests/Test_Utilities.cpp
@@ -2,3 +2,17 @@
 // Created by JakubSzwedowicz on 13.10.2021.
 //
 
+#include "gtest/gtest.h"
+#include "Utilities.hpp"
+#include "Table.hpp"
+
+TEST(Test_Utilities, test_should_allocate_2Dtable_5x3)
+{
+    int** table = NULL;
+    const int sizeX = 5;
+    const int sizeY = 3;
+    ASSERT_EQ(table, nullptr);
+    fun::alloc_2Dtable(&table, sizeX, sizeY);
+    ASSERT_NE(table, nullptr);
+    fun::dealloc_2Dtable(table, sizeX, sizeY);
+}
